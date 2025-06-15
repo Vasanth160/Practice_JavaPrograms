dc645b10ca0380952a77c1a3f6d59a9d67c3cfc3
vasan <vasan@Vasanth>
2025-06-15 23:00:09 +0530

    Added new Project

---
+++ JavaNewProject/src/Programs/Combain_FirstLettoerOfEachString.java
@@ -0,0 +1,30 @@
+package Programs;
+
+public class Combain_FirstLettoerOfEachString {
+
+	public static void main(String[] args) {
+		String text="Automation Test Engineer";
+
+		String[] split = text.split(" ");
+
+		int maxLength=0;
+
+		for (String word : split) {
+			if(word.length()>maxLength)
+				maxLength=word.length();
+		}
+		System.out.println(maxLength);
+
+		for(int i=0;i<maxLength;i++) {
+			for (String result : split) {
+				if(i<result.length())
+					System.out.print(result.charAt(i));
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
